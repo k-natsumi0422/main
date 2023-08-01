3a15c51cc2c7101fe39646b78f7a902f20d1b877
milk_ <milk_@LAPTOP-QOOAVH68>
2023-08-02 03:07:44 +0900

    Java1.22

--- HelloWorld_KameokaNatsumi/src/curriculum1_22B/Main1_22.java
+++ HelloWorld_KameokaNatsumi/src/curriculum1_22B/Main1_22.java
@@ -13,16 +13,16 @@ public class Main1_22 {
 		String[] checks = {"こんにちは！", "日本", "寿司", "和食"};
 		
 		// インスタンス化してコンストラクタ起動
-		Handler1_22 theme1 = new Handler1_22(checks);
+		Handler1_22 handler1_22 = new Handler1_22(checks);
 		
 		// SimpleDateFormatクラスを使用してフォーマットを指定
 		SimpleDateFormat dateTime = new SimpleDateFormat("'今の現在日時は'yyyy/MM/dd HH:mm:ss'です'");
 
 		// コンソール出力
 		System.out.println(
-			theme1.getGreeting() + "ここは" + theme1.getCountry() + "です！\n"
-			+ "この" + theme1.getFood() + "はうまい\n"
-			+ theme1.getFood() + "は" + theme1.getFoodType() + "です\n"
+				handler1_22.getGreeting() + "ここは" + handler1_22.getCountry() + "です！\n"
+			+ "この" + handler1_22.getFood() + "はうまい\n"
+			+ handler1_22.getFood() + "は" + handler1_22.getFoodType() + "です\n"
 			+ dateTime.format(new Date())
 		);
 
