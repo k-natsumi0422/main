2eb77947d725fecbdfdc13b2e2d0916a8bf14459
milk_ <milk_@LAPTOP-QOOAVH68>
2023-08-02 03:08:59 +0900

    Java1.23

--- HelloWorld_KameokaNatsumi/src/curriculum1_23/Main1_23.java
+++ HelloWorld_KameokaNatsumi/src/curriculum1_23/Main1_23.java
@@ -4,18 +4,18 @@ public class Main1_23 {
 
 	public static void main(String[] args) {
 		// インスタンス化
-		Handler1_23 theme5 = new Handler1_23();
+		Handler1_23 handler1_23 = new Handler1_23();
 		
 		// セッターに値を代入
-		theme5.setAnimal("ライオン");
-		theme5.setHeight(2.1);
-		theme5.setSpeed(80);
+		handler1_23.setAnimal("ライオン");
+		handler1_23.setHeight(2.1);
+		handler1_23.setSpeed(80);
 		
 		// コンソール出力
 		System.out.println(
-			"動物名：" + theme5.getAnimal() 
-			+ "\n体長：" + theme5.getHeight() + "m"
-			+ "\n速度：" + theme5.getSpeed() + "km/h"
+			"動物名：" + handler1_23.getAnimal() 
+			+ "\n体長：" + handler1_23.getHeight() + "m"
+			+ "\n速度：" + handler1_23.getSpeed() + "km/h"
 		);
 
 	}
