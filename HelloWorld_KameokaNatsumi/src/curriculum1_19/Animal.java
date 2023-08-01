e81dd0eb22e76a9ee6b2d7593f479807b78f12c4
milk_ <milk_@LAPTOP-QOOAVH68>
2023-08-01 18:06:00 +0900

    Java1.19

--- HelloWorld_KameokaNatsumi/src/curriculum1_19/Animal.java
+++ HelloWorld_KameokaNatsumi/src/curriculum1_19/Animal.java
@@ -7,11 +7,24 @@ public class Animal {
 	
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		// インスタンスの生成
+		Dog dog = new Dog("犬");
+		
+		// 犬
+		System.out.println(dog.animalName);
+		
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		Dog animals = new Dog("犬" , 4); 
-		System.out.println(animals.animalName + "\n" + animals.numberOfAnimal);
+		// インスタンスの生成
+		Dog dog2 = new Dog(4);
+		
+		//　4
+		System.out.println(dog2.numberOfAnimal);
+		
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-		SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd H:m:s");
-		System.out.println(time.format(new Date()));
+		// SimpleDateFormatクラスを使用してフォーマットを指定
+		SimpleDateFormat dateTime = new SimpleDateFormat("yyyy-MM-dd H:m:s");
+		
+		// 現在時刻
+		System.out.println(dateTime.format(new Date()));
 	}
 }
--- HelloWorld_KameokaNatsumi/src/curriculum1_19/Dog.java
+++ HelloWorld_KameokaNatsumi/src/curriculum1_19/Dog.java
@@ -2,15 +2,21 @@ package curriculum1_19;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	String animalName;
+	public String animalName;
+	
 	// Q2：フィールドに動物の数の変数を定義してください。
-	int numberOfAnimal;
+	public int numberOfAnimal;
+	
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	public Dog(String animalName, int numberOfAnimal) {
+	public Dog(String animalName) {
+		// インスタンス変数の参照
 		this.animalName = animalName;
+	}
+
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	public Dog(int numberOfAnimal) {
+		// インスタンス変数の参照
 		this.numberOfAnimal = numberOfAnimal;
 	}
-	
 	
 }
