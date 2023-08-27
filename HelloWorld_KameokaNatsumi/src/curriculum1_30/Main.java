ea311839e06077f3378c0596b2fe0b68e5709ae7
milk_ <milk_@LAPTOP-QOOAVH68>
2023-08-28 03:03:45 +0900

    Java1.30

---
+++ HelloWorld_KameokaNatsumi/src/curriculum1_30/Main.java
@@ -0,0 +1,9 @@
+package curriculum1_30;
+
+class Main {
+	public static void main(String[] args) {
+		// インスタンス化
+	    Person person1 = new Person("鈴木太郎", 20, 1.7,60);
+	    person1.print();
+	}
+}
---
+++ HelloWorld_KameokaNatsumi/src/curriculum1_30/Person.java
@@ -0,0 +1,36 @@
+package curriculum1_30;
+
+class Person{
+	// インスタンスフィールドを定義
+	public static int count;
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	
+	// bmiメソッドの定義
+	public double bmi() {
+		return this.weight / (height * height);
+	}
+	
+	// printメソッドの定義
+	public void print() {
+		// コンソール出力
+		System.out.println(
+				"名前は" + this.name + "です\n"
+				+ "年は" + this.age + "です\n"
+				+ "BMIは" + String.format("%.1f" , this.bmi()) + "です\n"
+				+ "合計" + Person.count + "人です\n"
+		);
+		
+	}
+
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	Person(String name, int age, double height, double weight) {
+		Person.count++;
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+}
